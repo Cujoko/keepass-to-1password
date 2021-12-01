de96c794717be2399a7278f2d88f62355a708d5c
Cujoko <cujoko@gmail.com>
2021-12-01 11:12:57 +0900

    .

--- k1p.py
+++ k1p.py
@@ -1,58 +1,83 @@
 #!/usr/bin/env python
-
-import os
-import logging
-
-import ConfigParser
+# -*- coding: utf-8 -*-
 import argparse
-from jinja2 import Environment, PackageLoader
+import logging
+import os
+
 from bs4 import BeautifulSoup
+import configparser
+import csv
+
 
 # Helper
 def normalize(s):
-  return '"%s"' % s.replace('"', '""')
-
-# Setup logging
-script_name = os.path.splitext(os.path.basename(__file__))[0]
-logging.basicConfig()
-logger = logging.getLogger(script_name)
-logger.setLevel(logging.DEBUG)
-
-# Parse command line options
-parser = argparse.ArgumentParser(
-    description='Converts KeePass XML file to 1Password CSV')
-parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
-args = parser.parse_args()
-
-# Read config file
-config = ConfigParser.SafeConfigParser()
-config.read(os.path.join('etc', script_name + '.conf'))
-
-passwords_xml = BeautifulSoup(open(config.get('General', 'input')))
-logger.info('KeePass XML file is opened')
-
-passwords = []
-
-for entry in passwords_xml.find_all('entry'):
-  password = {}
-  password['title'] = normalize(entry.title.string)
-  if entry.username.string:
-    password['username'] = normalize(entry.username.string)
-  if entry.password.string:
-    password['password'] = normalize(entry.password.string)
-  if entry.url.string:
-    password['url'] = normalize(entry.url.string.replace('http://', ''))
-  if entry.comment.contents:
-    # Convert <br> to line breaks:
-    notes = '\n'.join(unicode(element) for element in entry.comment.contents if element.name != 'br')
-    password['notes'] = normalize(notes)
-  passwords.append(password)
-
-# Prepare output file
-env = Environment(loader=PackageLoader('__main__', 'templates'))
-template = env.get_template('passwords.tmpl')
-output = open(config.get('General', 'output'), 'w')
-output.write(template.render(passwords = passwords).encode('utf-8'))
-output.close()
-
-logger.info('1Password CSV file is written')
+    return '"%s"' % s.replace('"', '""')
+    
+    
+def run() -> None:
+    # Setup logging
+    script_name = os.path.splitext(os.path.basename(__file__))[0]
+    logging.basicConfig()
+    logger = logging.getLogger(script_name)
+    logger.setLevel(logging.DEBUG)
+    
+    # Parse command line options
+    parser = argparse.ArgumentParser(
+        description='Converts KeePass XML file to 1Password CSV')
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
+    args = parser.parse_args()
+    
+    # Read config file
+    config = configparser.SafeConfigParser()
+    config.read(os.path.join('etc', script_name + '.conf'))
+    
+    passwords_xml = BeautifulSoup(open(config.get('General', 'input'), encoding='utf-8'))
+    logger.info('KeePass XML file is opened')
+    
+    fields = set()
+    entries = []
+    delimeter = ';'
+    
+    for entry_xml in passwords_xml.find_all('entry'):
+        entry = {}
+        
+        for string_xml in entry_xml.find_all('string'):
+            key = string_xml.key.string
+            value = string_xml.value.string
+            
+            if key and value:
+                key = key.replace(delimeter, '<delimeter>')
+                fields.add(key)
+                
+                value = value.replace(delimeter, '<delimeter>')
+                entry[key] = value
+                
+        # entry['title'] = normalize(entry_xml.title.string)
+        # if entry_xml.username.string:
+        #     entry['username'] = normalize(entry_xml.username.string)
+        # if entry_xml.password.string:
+        #     entry['password'] = normalize(entry_xml.password.string)
+        # if entry_xml.url.string:
+        #     entry['url'] = normalize(entry_xml.url.string.replace('http://', ''))
+        # if entry_xml.comment.contents:
+        #     # Convert <br> to line breaks:
+        #     notes = '\n'.join(element for element in entry_xml.comment.contents if element.name != 'br')
+        #     entry['notes'] = normalize(notes)
+            
+        entries.append(entry)
+        
+    # Prepare output file
+    with open(config.get('General', 'output'), 'w', encoding='utf-8') as output:
+        dialect = csv.excel
+        dialect.delimiter = delimeter
+        
+        writer = csv.DictWriter(output, fieldnames=fields, dialect=dialect)
+        writer.writeheader()
+        writer.writerows(entries)
+        
+    logger.info('1Password CSV file is written')
+    
+    
+if __name__ == '__main__':
+    run()
+    
